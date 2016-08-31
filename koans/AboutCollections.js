76302be603e42f1309c20ad38e4ee71902827f3d
Scott <sjs06e@gmail.com>
2016-08-31 20:33:45 +0000

    AboutCollections.js completed

--- koans/AboutCollections.js
+++ koans/AboutCollections.js
@@ -12,7 +12,7 @@ describe("Underscore Collection functions", function() {
       sum += item;
     });
 
-    expect(__).toEqual(sum);
+    expect(6).toEqual(sum);
   });
 
   it("_.each accepts a context as a third argument", function() {
@@ -20,7 +20,7 @@ describe("Underscore Collection functions", function() {
     add = function (item) { this.total += item; };
     _.each(collectionArray, add, context);
 
-    expect(__).toEqual(context.total);
+    expect(6).toEqual(context.total);
   });
   
   it("_.each shoud work with objects too", function() {
@@ -29,14 +29,14 @@ describe("Underscore Collection functions", function() {
       sum += item;
     });
 
-    expect(__).toEqual(sum);
+    expect(6).toEqual(sum);
   });
 
   it("_.map should transform a collection and return a new array of values", function() {
     var newArray = _.map(collectionArray, function(item) { return item*10; });
 
-    expect(__).toEqual(newArray);
-    expect(__).toEqual(collectionArray);
+    expect([10, 20, 30]).toEqual(newArray);
+    expect([1, 2, 3]).toEqual(collectionArray);
   });
 
   it("_.map should accept a context as a third argument", function() {
@@ -44,16 +44,16 @@ describe("Underscore Collection functions", function() {
     var multiply = function(item) { return item*this.factor; };
     newArray = _.map(collectionArray, multiply, context);
 
-    expect(__).toEqual(newArray);
+    expect([10, 20, 30]).toEqual(newArray);
   });
 
   it("_.pluck should return an array of property values from an array of objects", function() {
     var numerals = [{roman: "I", arabic: "1"}, 
                         {roman: "II", arabic: "2"}, 
                         {roman: "III", arabic: "3"}], selection;
-    selection = _.pluck(objectArray, "roman");
+    selection = _.pluck(numerals, "roman");
     
-    expect(__).toEqual(selection);
+    expect(["I", "II", "III"]).toEqual(selection);
   });
 
    it("_.every should return true if all members of a collection pass the criteria", function() {
@@ -63,7 +63,7 @@ describe("Underscore Collection functions", function() {
       isAboveThreshold = function (number) { return number > threshold; };
     result = _.every(multiplesOf100, isAboveThreshold );
     
-    expect(__).toEqual(result);
+    expect(true).toEqual(result);
   });
 
    it("_.every should return false if any member of a collection does not pass the criteria", function() {
@@ -73,7 +73,7 @@ describe("Underscore Collection functions", function() {
       isAboveThreshold = function (number) { return number > threshold; };
     result = _.every(multiplesOf10, isAboveThreshold );
     
-    expect(__).toEqual(result);
+    expect(false).toEqual(result);
   });
 
    it("_.every accepts a context as a third argument", function() {
@@ -83,7 +83,7 @@ describe("Underscore Collection functions", function() {
       isAboveThreshold = function (number) { return number > this.threshold; };
     result = _.every(multiplesOf10, isAboveThreshold, context );
     
-    expect(__).toEqual(result);
+    expect(false).toEqual(result);
   });
   
   
